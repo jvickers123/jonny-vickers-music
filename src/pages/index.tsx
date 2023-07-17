2e84b005057d8caa73310bdd44d7dede43524cfc
Jonny Vickers <jonathanbvickers@gmail.com>
2023-07-17 13:50:12 +0100

    remove global up css files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
 
-import albumArtNoText from '../assets/album-art-no-border-no-text.jpeg'
-
-const inter = Inter({ subsets: ['latin'] })
+import albumArtNoText from '../assets/album-art-no-border-no-text.jpeg';
 
 export default function Home() {
   return (
@@ -16,12 +12,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className='background'>
-          <Image alt='' fill src={albumArtNoText} objectFit='cover'/>
+      <main className="main">
+        <div className="background">
+          <Image alt="" fill src={albumArtNoText} objectFit="cover" />
         </div>
         <h1>Jonny Vickers</h1>
       </main>
     </>
-  )
+  );
 }
